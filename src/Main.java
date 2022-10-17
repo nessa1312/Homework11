bacd61e2cdc660b6f024e321bd064a3f170a12f6
vanessa <vanesska_denis2011@mail.ru>
2022-10-17 16:17:03 +0300

    The first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
 
+        System.out.println();
+        // Задание 2
+        System.out.println("Задание 2");
+        String fullName1 = "Ivanov Ivan Ivanovich";
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника — " + upperFullName);
+
+        System.out.println();
+        // Задание 3
+        System.out.println("Задание 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        String fixedFullName = fullName2.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fixedFullName);
     }
 }
